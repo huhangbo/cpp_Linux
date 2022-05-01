95e52495f7d7c06253770cb9754890337598c363
huhangbo <1746982699@qq.com>
2022-05-01 18:15:45 +0800

    update

--- net/client.cpp
+++ net/client.cpp
@@ -2,28 +2,19 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-
-
-
 int main() {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
-
     sockaddr_in address;
-
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(9999);
-
     inet_pton(AF_INET, "127.0.0.1", &address.sin_addr.s_addr);
     int ret = connect(fd, (sockaddr* )&address, sizeof(address));
-    if(ret == -1) {
-        perror("connect");
-        return -1;
-    }
+    int i = 0;
     while (true) {
-        char* data = "hello, I am client";
+        char data[128];
+        sprintf(data, "hello, I am client, data: %d", i++);
         write(fd, data, strlen(data));
-
         char recvBuf[1024] = {0};
         int len = read(fd, recvBuf, sizeof(recvBuf));
         if(len == -1) {
